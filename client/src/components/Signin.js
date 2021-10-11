05f48ecc00f947a31da4f90846655aa26debe902
Mrinal Seth <mrinalseth3959@gmail.com>
2021-10-12 00:35:03 +0530

    sign in completed

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {setAuthToken} from '../setTokenAuth'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
 import './signin.css'
 
 const Signin = () => {
+    const [email ,setEmail] = useState('')
+    const [password, setpassword] = useState('')
+    const [user, setUser] = useState('')
+    const onSubmit = async(e) => {
+        e.preventDefault()
+        const user = {
+            email,
+            password
+        }
+        try {
+            const res = await axios.post('/api/user/login', user)
+            console.log(res.data)
+            setUser(res.data.name)
+            localStorage.setItem('jwtToken', res.data.token)
+            setAuthToken(res.data.token)
+            const decoded = jwt_decode(res.data.token)
+        }catch (err) {
+            console.log(err.response)
+        }
+    }
     return (
-        <div>
-            sign in page
+        <div className="__signin">
+        {user}
+        <form class="form" onSubmit={onSubmit}>
+            <h2>Sign in</h2>
+            <div className="form-control">
+                <label for="email">Email</label>
+                <input 
+                    required
+                    type="text" 
+                    id="email" 
+                    placeholder="Enter email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div className="form-control">
+                <label for="password">Password</label>
+                <input 
+                    required
+                    type="password"
+                    id="password" 
+                    placeholder="Enter password"
+                    value={password} 
+                    onChange={(e) => {setpassword(e.target.value)}}/>
+            </div>
+            <button>Submit</button>
+        </form>
         </div>
     )
 }
