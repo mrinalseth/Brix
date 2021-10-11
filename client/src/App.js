03ad532e5fc6082ad36d8ae30ac61d46500ad4ef
Mrinal Seth <mrinalseth3959@gmail.com>
2021-10-12 01:31:31 +0530

    dashboard started

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from './components/Nav'
 import Signin from './components/Signin'
 import Signup from './components/Signup'
+import Dashboard from './components/Dashboard'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './app.css'
 
@@ -12,6 +13,7 @@ const App = () => {
       <Router>
         <Route exact path='/signin' component={Signin} />
         <Route exact path='/signup' component={Signup} />
+        <Route exact path='/dashboard' component={Dashboard} />
       </Router>
     </div>
   )
